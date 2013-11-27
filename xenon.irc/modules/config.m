97293285da7483f7e2cd8b0c8f480df0db24edba
hclinux <info@hacker-community.org>
2013-11-27 07:36:14 +0700

    Irc Script for BitchX
    bitchX-75+linux 2.2.2

---
+++ xenon.irc/modules/config.m
@@ -0,0 +1,15 @@
+### config.m - folk@lpsg.org
+##
+# to change color scheme make color.m and change the ansi vars to different colors
+#  e.x. @cyn=[ansi_code_for+red]
+alias config if (![$0]) {
+echo $G ${prp}ษ${hwht}x${cyn}enon${prp}อออออออออออออออออออออ${hwht}c${cyn}urrently ${hblk}[${hwht}$nickc.no${hblk}]${prp}อออออออป
+echo $G ${prp}ฬอออออออออออออออออออออออออออออออออออออออออออออออน
+echo $G ${prp}บ ${hcyn}e${cyn}mail address${prp}!${wht}$emailaddy                   ${prp}บ
+echo $G ${prp}บ ${hcyn}f${cyn}ormat selection${prp}!${wht}$xformat                      ${prp}บ
+echo $G ${prp}บ ${cyn}ctcp cloa${hcyn}k${prp}!${wht}$xcloak                                ${prp}บ 
+echo $G ${prp}บ ${hcyn}p${cyn}rompt charachter${prp}!${cyn}$G                         ${prp}บ
+echo $G ${prp}บ ${cyn}htm${hcyn}l${cyn} away${hwht}: ${hblk}[${cyn}$htmlaway${hblk}]${prp}                                 ${prp}บ
+echo $G ${prp}บ ${hcyn}c${cyn}olor scheme${hwht}: ${hblk}[${cyn}$xscheme${hblk}]${prp}                              ${prp}บ
+echo $G ${prp}ศอออออออออออออออออออออออออออออออออออออออออออออออผ
+}
---
+++ xenon.irc/modules/xhelp.m
@@ -0,0 +1,16 @@
+### xhelp.m - folk@lpsg.org
+##
+# help!@##
+alias xhelp if (![$0]) {
+echo $G ${prp}ษ${hwht}x${cyn}enon${prp} อออออออออออออออออออ${hwht}c${cyn}urrently ${hblk}[${hwht}$nickc.no${hblk}]${prp}อออออออป
+echo $G ${prp}ฬออออออออออออออออออออออออออออออออออออออออออออออน
+echo $G ${prp}บ ${hcyn}a${cyn}way${prp}!${cyn}/away, /back                            ${prp}บ 
+echo $G ${prp}บ ${hcyn}w${cyn}indow join${prp}!${cyn}/wj                              ${prp}บ 
+echo $G ${prp}บ ${cyn}nick com${hcyn}p${prp}!${cyn}/xnick                             ${prp}บ  
+echo $G ${prp}บ ${hcyn}c${cyn}onfigure${prp}!${cyn}/config                            ${prp}บ
+echo $G ${prp}บ ${cyn}ctc${hcyn}p${cyn}${prp}!${cyn}/ctcp                                   ${prp}บ     
+echo $G ${prp}บ ${hcyn}c${cyn}dcc scheme${prp}!${cyn}/cdcc to get another long list   ${prp}บ
+echo $G ${prp}บ ${cyn}xnic${hcyn}k${cyn}${prp}!${cyn}/xnick                                 ${prp}บ
+echo $G ${prp}ศออออออออออออออออออออออออออออออออออออออออออออออผ
+}
+
